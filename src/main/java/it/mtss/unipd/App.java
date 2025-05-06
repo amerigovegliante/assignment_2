4c7f614312b34b1444b4783cdaaebaf1bbe833db
amerigovegliante <veglianteamerigo583@gmail.com>
2025-05-06 19:44:54 +0200

    close #1

---
+++ src/main/java/it/mtss/unipd/App.java
@@ -0,0 +1,13 @@
+////////////////////////////////////////////////////////////////////
+// [Amerigo] [Vegliante] [2111004]
+// [Giulia] [Romanato] [2104060]
+///////////////////////////////////////////////////////////////////
+package it.mtss.unipd;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
