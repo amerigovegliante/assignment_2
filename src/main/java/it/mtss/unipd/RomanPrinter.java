3f46874e51fce0bd05049eb49d5c7872bbca5c97
amerigovegliante <veglianteamerigo583@gmail.com>
2025-05-09 19:07:18 +0200

    close #17

--- src/main/java/it/mtss/unipd/RomanPrinter.java
+++ src/main/java/it/mtss/unipd/RomanPrinter.java
@@ -15,6 +15,7 @@ public class RomanPrinter {
       String[] AsciiArt_I = {" _____ ","|_   _|","  | |  ","  | |  "," _| |_ ","|_____|"};
       String[] AsciiArt_V = {"__      __", "\\ \\    / /", " \\ \\  / / ","  \\ \\/ /  ","   \\  /   ","    \\/    "};
       String[] AsciiArt_X={"__    __","\\ \\  / /"," \\ \\/ / ","  >  <  "," / /\\ \\ ","/_/  \\_\\"};
+      String[] AsciiArt_L ={" _      ","| |     ","| |     ","| |     ","| |____ ","|______|"};
       for (int j=0;j<6;j++){
           for (int i=0;i<Roman.length;i++){
             if(Roman[i]=='I'){
@@ -25,8 +26,11 @@ public class RomanPrinter {
               AsciiArt+=AsciiArt_V[j];
             }else if(Roman[i]=='X'){
               AsciiArt=AsciiArt+AsciiArt_X[j];
+            }else if(Roman[i]=='L')
+            {
+                AsciiArt+=AsciiArt_L[j];
             }
-
+            
             if(i!=Roman.length-1){
                 AsciiArt=AsciiArt+" ";
             }
