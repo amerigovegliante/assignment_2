2570b2df6a4e26ec672b7b65017e73b8e6daf19a
Giulia2424 <giuliarom4@gmail.com>
2025-05-09 18:03:41 +0200

    close #12

--- src/main/java/it/mtss/unipd/IntegerToRoman.java
+++ src/main/java/it/mtss/unipd/IntegerToRoman.java
@@ -3,18 +3,25 @@ package it.mtss.unipd;
 public class IntegerToRoman {
     public static String convert(int number){
         String roman=new String();
-        if(number<=3){
+        if(number<0 || number>1000){
+            return null;
+        }else if(number<=3){
             for (int i=0;i<number;i++){
                 roman=roman+"I";
             }
-        } else if(number <= 6)
-        {
-            if(number == 4)
-                roman = "IV";
-            else if(number == 5)
-                roman = "V";
-            else
-                roman = "VI";
+        } else if(number<=8){
+            if(number==4){
+                roman="IV";
+            }else{
+                roman="V";
+                for (int i=0;i<number-5;i++){
+                roman=roman+"I";
+               }
+            }   
+        }else if (number<=10) {
+            if(number==9){
+                roman="IX";
+            }else{roman="X";}
         }
         return roman;
     }
